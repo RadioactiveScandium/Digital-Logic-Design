e1b686b0cd598b397c0fdbe4e27efd3a85e2d38d
Saransh Choudhary <scsc2699@gmail.com>
2024-07-06 16:08:26 +0530

    Resolution to issue number 13 - "Scale the Down Counter code"

--- Implementation and Results/Binary counters/Down Counters/Four_bit_DOWN_counter.sv
+++ Implementation and Results/Binary counters/Down Counters/Four_bit_DOWN_counter.sv
@@ -1,14 +1,14 @@
-`timescale 1ns/1ps                // to ensure tight compatibility between TB and design timescales
-`default_nettype none
-module Four_bit_DOWN_counter(
-                                  input logic clk,
-                                  input logic rst,
-                                  output logic [3:0] out  
-                          );                       
+module Four_bit_DOWN_counter #(parameter MOD_VALUE = 32) (
+                                  input  logic 							         clk,
+                                  input  logic 							         rst,
+  								          output logic [($clog2(MOD_VALUE)-1):0]   out  
+                             );                       
 
-  always_ff @ (posedge clk or negedge rst) begin  // few compilers like Icarus don't understand always_ff statement, so use always in such cases
+  
+  
+always_ff @ (posedge clk or negedge rst) begin  // few compilers like Icarus don't understand always_ff statement, so use always in such cases
       if (~rst)   
-         out <= 'b1;
+         out <= '1;
       else begin
          out <= out - 1;
       end
