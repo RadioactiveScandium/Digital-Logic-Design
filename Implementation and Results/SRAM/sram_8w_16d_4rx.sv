8a4ec12134e26f7c96c80a3cd0049bc2fd2d6125
Saransh Choudhary <scsc2699@gmail.com>
2025-02-20 00:20:28 +0530

    Revamped the code for SRAM

--- Implementation and Results/SRAM/sram_8w_16d_4rx.sv
+++ Implementation and Results/SRAM/sram_8w_16d_4rx.sv
@@ -1,92 +1,30 @@
-/*
-8-bit sram, with 16 depth and 4 bits reserved. Below are the parameters that represent this :
+module sram #( 
+                parameter int WIDTH = 32,
+                parameter int DEPTH = 1024
+             ) 
+             ( 
+                input  logic                     rstn,  
+                input  logic                     clk,  
+                input  logic                     wren,  
+                input  logic                     rden,  
+                input  logic [$clog2(DEPTH)-1:0] addr,
+                input  logic [WIDTH-1:0]         wr_data,
+                output logic [WIDTH-1:0]         rd_data
+             );
 
-DEPTH = 16 ; WIDTH = 8 ; RESERVED = 4 (MSBs) 
-*/
+logic [DEPTH-1:0][WIDTH-1:0] sram;
 
-module sram_8w_16d_4rx #(parameter WIDTH = 8, DEPTH = 16, ADDR = 16) (
-                                 input logic                  clk,      
-                                 input logic                  rstn,      
-                                 input logic                  wren,      
-                                 input logic                  rden,      
-                                 input logic                  boot_mode,      
-                                 input logic                  train_mode,
-                                 input logic  [WIDTH-1:0]     data_in, 
-                                 input logic  [ADDR-1:0]      addr,
-                                 output logic [WIDTH-1:0]     data_out 
-                         );
-
-bit   [DEPTH-1:0][WIDTH-1:0]   sram;
-logic [($clog2(WIDTH)+1):0]    ones_count_top;
-logic [WIDTH-1:0]              data_out_ff; 
-logic [WIDTH-1:0]              data_out_xor; 
-logic [WIDTH-1:0]              data_out_xor_ff; 
-logic count_GT_1;
-
-always_ff @ (posedge clk or negedge rstn) begin
-      if (~rstn) begin 
-                data_out <= 'h0;
-                sram     <= 'h0; 
-      end
-
-      else begin
-            /* If not in boot mode, write to specific locations in the SRAM */
-               if (boot_mode == 1'b0) begin 
-                              if (wren && ~rden) begin
-                              //if (wren == 1'b1 && rden == 1'b0) begin
-                                       /* ENHANCEMENT : Add a feature where the user can select between
-                                        generating a pattern like below or writing a custom data  */
-                                          if (train_mode) begin
-                                             for (integer i = 0 ; i < DEPTH ; i = i+1) begin : PATTERN_GENERATOR 
-                                                   sram[i] <= i+1 ;
-                                             end
-                                          end
-                                          else
-                                             sram[addr] <= data_in;
-                              end
-              end
-
-            /* If in boot mode, read from desired locations*/
-              else begin
-                              if (wren == 1'b0 && rden == 1'b1)
-                                        data_out <= sram[addr];
-              end
-
-     end 
-
-end
-
-//always_ff @ (posedge clk or negedge rstn) begin
-//
-//      if (~rstn)
-//                data_out_ff <= 'h0;
-//      else  
-//                data_out_ff <= data_out;
-//end
-
-CountOnes CountOnes_8w_16d_4rx (
-                                 .ones_count(ones_count_top),
-                                 .binary_data(data_out)
-                               );
-
-always_comb begin
-   count_GT_1  = 1'b0;
-   if (ones_count_top > 1)
-       count_GT_1 = 1'b1;
-end
-
-always_comb begin
-   data_out_xor = 0;
-   if (count_GT_1) 
-       data_out_xor = data_out ^ data_in ; 
-end
-
-/* Registering the XOR output to delay it by one clock cycle */
-always_ff @ (posedge clk or negedge rstn) begin
-      if (~rstn)
-                data_out_xor_ff <= 'h0;
-      else  
-                data_out_xor_ff <= data_out_xor;
+always_ff@(posedge clk or negedge rstn) begin
+    if(~rstn)
+        {sram,rd_data} <= {0,{WIDTH{1'h0}}};
+    else begin
+        case({wren,rden})
+            2'b00 : sram[addr] <= sram[addr];     // No Operation
+            2'b01 : rd_data    <= sram[addr];     // Write Operation 
+            2'b10 : sram[addr] <= wr_data;        // Read Operation
+            2'b11 : sram[addr] <= {WIDTH{1'h0}};  // Corrupting the memory by writing all zeros if read / write is attempted at a single address at the same instant
+        endcase
+    end
 end
 
 endmodule
--- Implementation and Results/SRAM/sram_8w_16d_4rx_tb.sv
+++ Implementation and Results/SRAM/sram_8w_16d_4rx_tb.sv
@@ -1,28 +1,24 @@
 `timescale 1ns/1ps
-module sram_8w_16d_4rx_test #(parameter WIDTH = 8, DEPTH = 16, ADDR = 16) ();
+module sram_test #( parameter int WIDTH = 32, parameter int DEPTH = 1024) ();
 
-logic                  clk;      
-logic                  rstn;      
-logic                  wren;      
-logic                  rden;      
-logic                  boot_mode; 
-logic                  train_mode;
-logic [WIDTH-1:0]      data_in; 
-logic [ADDR-1:0]       addr;
-logic [WIDTH-1:0]      data_out; 
-                     
-  
-sram_8w_16d_4rx sram_8w_16d_4rx_dut (
-                                             .rstn(rstn),
-                                             .clk(clk),
-                                             .wren(wren),
-                                             .rden(rden),
-                                             .boot_mode(boot_mode),
-                                             .train_mode(train_mode),
-                                             .data_in(data_in),
-                                             .addr(addr),
-                                             .data_out(data_out)
-                                   );
+logic                     rstn;  
+logic                     clk;  
+logic                     wren;  
+logic                     rden;  
+logic [$clog2(DEPTH)-1:0] addr;
+logic [WIDTH-1:0]         wr_data;
+logic [WIDTH-1:0]         rd_data;
+
+sram #(.WIDTH(32), .DEPTH(1024)) dut
+             ( 
+                .rstn(rstn),
+                .clk(clk),
+                .wren(wren),
+                .rden(rden),
+                .addr(addr),
+                .wr_data(wr_data),
+                .rd_data(rd_data)
+             );
 
 always #5 clk = ~clk;
   
@@ -31,64 +27,81 @@ initial begin
    		clk     = 1'b0; 
   		wren    = 1'b0;
   		rden    = 1'b0;
-        boot_mode = 1'b0;
-        train_mode = 1'b0;
-  		data_in = 8'h0;
-  		addr    = 16'h0;
+  		wr_data = 32'h0;
+  		addr    = 10'b0; #200;
 end	
 
 initial begin
-  $fsdbDumpvars("+fsdbfile+async_SRAM_test_8w_16d.fsdb","+all");
+  $fsdbDumpvars("+fsdbfile+sram_test.fsdb","+all");
   
   /* Releasing the reset */
   #100 rstn = 1'b1; 
 
-  /* Not in boot mode - WR operation */ 
+  /* Write mode */
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h1, 10'h0}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h2, 10'h1}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h3, 10'h2}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h4, 10'h3}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h5, 10'h4}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h6, 10'h5}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h7, 10'h6}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h8, 10'h7}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h9, 10'h8}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h10, 10'h9}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h11, 10'hA}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h12, 10'hB}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h13, 10'hC}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h14, 10'hD}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'h15, 10'hE}; #50;
+  {wren, rden, wr_data, addr} = {1'b1, 1'b0, 32'hAA, 10'h3FF}; #50;
 
-  /* Training mode */ 
-  boot_mode = 1'b0; train_mode = 1'b1; wren = 1'b1; #405;
+  /* Read mode */
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h0}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h1}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h2}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h3}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h4}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h5}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h6}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h7}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h8}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h9}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hA}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hB}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hC}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hD}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hE}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h3FF}; #50;
 
-  /* Transaction mode */ 
-  boot_mode = 1'b0; train_mode = 1'b0; wren = 1'b1; #205;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h1, 16'h0}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h2, 16'h1}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h3, 16'h2}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h4, 16'h3}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h5, 16'h4}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h6, 16'h5}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h7, 16'h6}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h8, 16'h7}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h9, 16'h8}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'hA, 16'h9}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'hB, 16'hA}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'hC, 16'hB}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'hD, 16'hC}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'hE, 16'hD}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'hF, 16'hE}; #50;
-  {wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h10, 16'hF}; #50;
-  //{wren, rden, data_in, addr} = {1'b1, 1'b0, 8'h11, 16'h10}; #50; // Invalid Address // 
+  #100;
 
-  /* In boot mode - RD operation */ 
-   boot_mode = 1'b1; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h0}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h1}; #50;
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h2}; #50;
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h3}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h4}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h5}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h6}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h7}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h8}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'h9}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'hA}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'hB}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'hC}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'hD}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'hE}; #50; 
-  {wren, rden, addr} = {1'b0, 1'b1, 16'hF}; #50; 
-  //{wren, rden, addr} = {1'b0, 1'b1, 16'h10}; #50; // Invalid Address // 
-  #1000 $finish;	
+  /* Partial memory corruption test */
+  {wren, rden, addr} = {1'b1, 1'b1, 10'h0}; #50;
+  {wren, rden, addr} = {1'b1, 1'b1, 10'h1}; #50;
+  {wren, rden, addr} = {1'b1, 1'b1, 10'h2}; #50;
+  {wren, rden, addr} = {1'b1, 1'b1, 10'h3}; #50;
+  {wren, rden, addr} = {1'b1, 1'b1, 10'h4}; #50;
+  {wren, rden, addr} = {1'b1, 1'b1, 10'h5}; #50;
+  {wren, rden, addr} = {1'b1, 1'b1, 10'h6}; #50;
 
+  /* Read mode */
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h0}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h1}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h2}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h3}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h4}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h5}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h6}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h7}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h8}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h9}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hA}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hB}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hC}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hD}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'hE}; #50;
+  {wren, rden, addr} = {1'b0, 1'b1, 10'h3FF}; #50;
+  $finish;
 end
 
-endmodule  
+endmodule
