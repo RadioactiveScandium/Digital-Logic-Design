4f7e6124858ef7ac64d2582690650d32c37a1a46
Saransh Choudhary <scsc2699@gmail.com>
2026-07-31 17:23:20 +0530

    Create demo.sv

---
+++ SV code examples/demo.sv
@@ -0,0 +1,14 @@
+module demo ( 
+   input logic a,
+  output logic b,
+  input logic clk,
+  input logic rstn
+       );
+
+  always_ff @ (posedge clk or negedge rstn) begin
+    if (~rstn) b <= 1'b0;
+  else b = a;
+  end
+
+  
+endmodule
